b8be73face69f1f26e9be21b964cdec4f9db463e
levion8304 <ngocliem10a5nth@gmail.com>
2024-11-07 17:59:58 +0700

    Chỉnh middleware

--- backend/api/v1/middlewares/client/auth.middleware.js
+++ backend/api/v1/middlewares/client/auth.middleware.js
@@ -1,34 +1,20 @@
+const { cookies } = require("../../../../helpers/cookies");
 const User = require("../../models/user.model");
 
 module.exports.requireAuth = async (req, res, next) => {
+  const token = cookies(req).token;
 
-  if (req.headers.authorization) {
-    const token = req.headers.authorization.split(" ")[1];
+  const user = await User.findOne({
+    token: token,
+  }).select("-password -token");
 
-    const user = await User.findOne({
-      token: token,
-    })
-      .select("-password -token");
-
-    if (!user) {
-      res.json({
-        code: 400,
-        message: "Invalid Token"
-      })
-      return;
-    }
-    
-    res.json({
-      code: 200,
-      message: "Access Success"
-    })
-
-    next();
-
-  } else {
+  if (!user) {
     res.json({
       code: 400,
-      message: "Access Denied"
-    })
+      message: "Invalid Token",
+    });
+    return;
   }
-}
+
+  next();
+};
--- backend/api/v1/middlewares/client/cart.middleware.js
+++ backend/api/v1/middlewares/client/cart.middleware.js
@@ -1,25 +1,24 @@
 const Cart = require("../../models/cart.model");
+const User = require("../../models/user.model");
+const { cookies } = require("../../../../helpers/cookies");
 
 module.exports.requireCart = async (req, res, next) => {
-  const cookies = req.headers.cookie.split("; ");
-  let cartIdExist = false;
-  for (const cookie of cookies) {
-    const key = cookie.split("=")[0];
-    if (key === 'cartId') {
-      cartIdExist = true;
-      break;
-    }
-  }
-  if (!cartIdExist)  {
-    const cart = new Cart();
+  const cartId = cookies(req).cartId;
+  if (!cartId) {
+    const token = cookies(req).token;
+    const user = await User.findOne({ token: token });
+    const cart = new Cart({
+      userId: user.id,
+    });
     await cart.save();
 
     const expiresTime = 1000 * 60 * 60 * 24 * 365;
 
+   
     res.cookie("cartId", cart.id, {
       expires: new Date(Date.now() + expiresTime),
     });
   }
 
   next();
-}
+};
