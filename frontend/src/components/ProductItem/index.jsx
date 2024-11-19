958388099651f2c7f68c84acb0ce08841d43d1c3
Ho Tran Ngoc Liem <ngocliem10a5nth@gmail.com>
2024-11-19 18:26:49 +0700

    Tạo Skeleton cho ProductItem và trang SearchPage

--- frontend/src/components/ProductItem/index.jsx
+++ frontend/src/components/ProductItem/index.jsx
@@ -4,51 +4,80 @@ import { Button, Card } from "react-bootstrap";
 import { Link, useNavigate } from "react-router-dom";
 import Rating from "react-rating";
 import "./ProductItem.css";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
-function ProductItem({ product }) {
+function ProductItem({ product, loading }) {
   const navigate = useNavigate();
   return (
-    <Card
-      className="product-card"
-      onClick={() => {
-        navigate(`/detailproduct/${product.id}`);
-      }}
-    >
-      <Card.Img
-        variant="top"
-        src={product.thumbnail_url || "/images/default/default-product.png"}
-      />
-      <Card.Body>
-        <Card.Title>{product.name}</Card.Title>
-        <Card.Text>
-          <span className="product-price">
-            {(
-              product.original_price *
-              (1 - product.discount_rate / 100)
-            ).toLocaleString()}
-            <sup>đ</sup>
-          </span>
-        </Card.Text>
-        <Card.Text className="product-info">
-          <span>
-            {product.rating_average || "0"}{" "}
-            <Rating
-              emptySymbol="fa-regular fa-star"
-              fullSymbol="fa-solid fa-star"
-              initialRating={product.rating_average}
-              readonly
-              style={{ color: "#dfdf29"}}
-            />
-          </span>{" "}
-          | Đã bán: {product.quantity_sold?.value || 0}
-        </Card.Text>
-        <Link to={`/detailproduct/${product.id}`}>
-          <Button variant="primary" className="w-100">
-            Xem Chi Tiết
-          </Button>
-        </Link>
-      </Card.Body>
-    </Card>
+    <>
+      {loading ? (
+        <Card
+          className="product-card"
+          onClick={() => {
+            navigate(`/detailproduct/${product.id}`);
+          }}
+        >
+          <Skeleton height={160} />
+          <Card.Body>
+            <Card.Title>
+              <Skeleton className="card-title" />
+            </Card.Title>
+            <Card.Text>
+              <Skeleton className="product-price" />
+            </Card.Text>
+            <Card.Text>
+              <Skeleton className="product-info" />
+            </Card.Text>
+            <Skeleton className="w-100 .btn" />
+          </Card.Body>
+        </Card>
+      ) : (
+        <Card
+          className="product-card"
+          onClick={() => {
+            navigate(`/detailproduct/${product.id}`);
+          }}
+        >
+          <Card.Img
+            variant="top"
+            src={product.thumbnail_url || "/images/default/default-product.png"}
+          />
+          <Card.Body>
+            <Card.Title>{product.name}</Card.Title>
+            <Card.Text>
+              <span className="product-price">
+                {(
+                  product.original_price *
+                  (1 - product.discount_rate / 100)
+                ).toLocaleString()}
+                <sup>đ</sup>
+              </span>
+            </Card.Text>
+            <Card.Text>
+              <div className="product-info">
+                <span>
+                  {product.rating_average || "0"}{" "}
+                  <Rating
+                    emptySymbol="fa-regular fa-star"
+                    fullSymbol="fa-solid fa-star"
+                    initialRating={product.rating_average}
+                    readonly
+                    style={{ color: "#dfdf29" }}
+                  />
+                </span>{" "}
+                | Đã bán: {product.quantity_sold?.value || 0}
+              </div>
+            </Card.Text>
+            <Link to={`/detailproduct/${product.id}`}>
+              <Button variant="primary" className="w-100">
+                Xem Chi Tiết
+              </Button>
+            </Link>
+          </Card.Body>
+        </Card>
+      )}
+    </>
   );
 }
 
--- frontend/src/pages/SearchPage/SearchPage.jsx
+++ frontend/src/pages/SearchPage/SearchPage.jsx
@@ -5,6 +5,7 @@ import { axiosApi } from "../../services/UserService";
 import ProductItem from "../../components/ProductItem";
 import { useNavigate } from "react-router-dom";
 import { animateScroll } from "react-scroll";
+import { Skeleton } from "antd";
 
 const SearchPage = () => {
   const [minPrice, setMinPrice] = useState("");
@@ -97,174 +98,222 @@ const SearchPage = () => {
 
   return (
     <>
-      {loading ? (
-        <div>Loading...</div>
-      ) : (
-        <div className="search-page container">
-          <Row>
-            {/* Bộ lọc tìm kiếm */}
-            <Col md={3}>
-              <div className="filters-section">
-                <h5>Bộ Lọc Tìm Kiếm</h5>
-                <Form>
-                  <Form.Group controlId="priceRange" className="mb-4">
-                    <Form.Label>Khoảng Giá</Form.Label>
-                    <div className="d-flex align-items-center mb-2">
-                      <Form.Control
-                        type="number"
-                        placeholder="Từ"
-                        value={minPrice}
-                        onChange={(e) => setMinPrice(e.target.value)}
-                        className="me-2"
-                        style={{ width: "45%" }}
-                      />
-                      <Form.Control
-                        type="number"
-                        placeholder="Đến"
-                        value={maxPrice}
-                        onChange={(e) => setMaxPrice(e.target.value)}
-                        style={{ width: "45%" }}
-                      />
-                    </div>
-                    <Button
-                      variant="danger"
-                      className="mt-2 w-100"
-                      onClick={handleApplyFilters}
-                    >
-                      Áp Dụng
-                    </Button>
-                  </Form.Group>
-
-                  <Form.Group controlId="storeType" className="mb-4">
-                    <Form.Label>Loại Shop</Form.Label>
-                    <Form.Check
-                      type="checkbox"
-                      label="Shopee Mall"
-                      onChange={() => toggleStoreType("Shopee Mall")}
-                      // checked={storeType.includes("Shopee Mall")}
+      <div className="search-page container">
+        <Row>
+          <Col md={3}>
+            <div className="filters-section">
+              <h5>Bộ Lọc Tìm Kiếm</h5>
+              <Form>
+                <Form.Group controlId="priceRange" className="mb-4">
+                  <Form.Label>Khoảng Giá</Form.Label>
+                  <div className="d-flex align-items-center mb-2">
+                    <Form.Control
+                      type="number"
+                      placeholder="Từ"
+                      value={minPrice}
+                      onChange={(e) => setMinPrice(e.target.value)}
+                      className="me-2"
+                      style={{ width: "45%" }}
                     />
-                    <Form.Check
-                      type="checkbox"
-                      label="Shop Yêu Thích"
-                      onChange={() => toggleStoreType("Shop Yêu Thích")}
-                      // checked={storeType.includes("Shop Yêu Thích")}
+                    <Form.Control
+                      type="number"
+                      placeholder="Đến"
+                      value={maxPrice}
+                      onChange={(e) => setMaxPrice(e.target.value)}
+                      style={{ width: "45%" }}
                     />
-                    <Form.Check
-                      type="checkbox"
-                      label="Xử lý đơn hàng bởi Shopee"
-                      onChange={() => toggleStoreType("Shopee")}
-                      // checked={storeType.includes("Shopee")}
-                    />
-                  </Form.Group>
-
-                  <Form.Group controlId="productCondition" className="mb-4">
-                    <Form.Label>Tình Trạng</Form.Label>
-                    <Form.Check
-                      type="checkbox"
-                      label="Đã sử dụng"
-                      onChange={() => toggleProductCondition("Đã sử dụng")}
-                      // checked={productCondition.includes("Đã sử dụng")}
-                    />
-                    <Form.Check
-                      type="checkbox"
-                      label="Mới"
-                      onChange={() => toggleProductCondition("Mới")}
-                      // checked={productCondition.includes("Mới")}
-                    />
-                  </Form.Group>
-
-                  <Form.Group controlId="rating" className="mb-4">
-                    <Form.Label>Đánh Giá</Form.Label>
-                    {[5, 4, 3].map((star) => (
-                      <Form.Check
-                        key={star}
-                        type="checkbox"
-                        label={`${"⭐️".repeat(star)} trở lên`}
-                        onChange={() => setRatingFilter(star)}
-                        checked={ratingFilter === star}
-                      />
-                    ))}
-                  </Form.Group>
-
-                  <Form.Group controlId="promotion" className="mb-4">
-                    <Form.Label>Dịch Vụ & Khuyến Mãi</Form.Label>
-                    <Form.Check
-                      type="checkbox"
-                      label="Voucher Xtra"
-                      onChange={() => togglePromotion("Voucher Xtra")}
-                      checked={promotions.includes("Voucher Xtra")}
-                    />
-                    <Form.Check
-                      type="checkbox"
-                      label="Đang giảm giá"
-                      onChange={() => togglePromotion("Đang giảm giá")}
-                      checked={promotions.includes("Đang giảm giá")}
-                    />
-                    <Form.Check
-                      type="checkbox"
-                      label="Hàng có sẵn"
-                      onChange={() => togglePromotion("Hàng có sẵn")}
-                      checked={promotions.includes("Hàng có sẵn")}
-                    />
-                  </Form.Group>
-
+                  </div>
                   <Button
-                    variant="secondary"
-                    className="mt-3 w-100"
-                    onClick={handleResetFilters}
+                    variant="danger"
+                    className="mt-2 w-100"
+                    onClick={handleApplyFilters}
                   >
-                    Xóa Tất Cả
+                    Áp Dụng
                   </Button>
-                </Form>
-              </div>
-            </Col>
+                </Form.Group>
 
-            {/* Kết quả tìm kiếm */}
-            <Col md={9}>
-              <Row className="align-items-center justify-content-between mb-3">
-                <h5>Kết quả tìm kiếm cho "{keyword}"</h5>
-                <Dropdown onSelect={(e) => handleSortChange(e)}>
-                  <Dropdown.Toggle variant="outline-primary">
-                    {/* {sortBy} */}
-                  </Dropdown.Toggle>
-                  <Dropdown.Menu>
-                    <Dropdown.Item eventKey="Liên Quan">
-                      Liên Quan
-                    </Dropdown.Item>
-                    <Dropdown.Item eventKey="Giá: Từ Thấp đến Cao">
-                      Giá: Từ Thấp đến Cao
-                    </Dropdown.Item>
-                    <Dropdown.Item eventKey="Giá: Từ Cao đến Thấp">
-                      Giá: Từ Cao đến Thấp
-                    </Dropdown.Item>
-                  </Dropdown.Menu>
-                </Dropdown>
-              </Row>
+                <Form.Group controlId="storeType" className="mb-4">
+                  <Form.Label>Loại Shop</Form.Label>
+                  <Form.Check
+                    type="checkbox"
+                    label="Shopee Mall"
+                    onChange={() => toggleStoreType("Shopee Mall")}
+                    // checked={storeType.includes("Shopee Mall")}
+                  />
+                  <Form.Check
+                    type="checkbox"
+                    label="Shop Yêu Thích"
+                    onChange={() => toggleStoreType("Shop Yêu Thích")}
+                    // checked={storeType.includes("Shop Yêu Thích")}
+                  />
+                  <Form.Check
+                    type="checkbox"
+                    label="Xử lý đơn hàng bởi Shopee"
+                    onChange={() => toggleStoreType("Shopee")}
+                    // checked={storeType.includes("Shopee")}
+                  />
+                </Form.Group>
 
-              {/* Danh sách sản phẩm */}
-              <Row xs={1} md={4} className="g-3">
-                {products && (
-                  <>
-                    {products.map((product) => (
-                      <Col
-                        key={product.id}
-                        style={{
-                          padding: 0,
-                          display: "flex",
-                          alignItems: "center",
-                          justifyContent: "center",
-                        }}
-                      >
-                        <ProductItem product={product} />
-                      </Col>
-                    ))}
-                  </>
-                )}
-              </Row>
+                <Form.Group controlId="productCondition" className="mb-4">
+                  <Form.Label>Tình Trạng</Form.Label>
+                  <Form.Check
+                    type="checkbox"
+                    label="Đã sử dụng"
+                    onChange={() => toggleProductCondition("Đã sử dụng")}
+                    // checked={productCondition.includes("Đã sử dụng")}
+                  />
+                  <Form.Check
+                    type="checkbox"
+                    label="Mới"
+                    onChange={() => toggleProductCondition("Mới")}
+                    // checked={productCondition.includes("Mới")}
+                  />
+                </Form.Group>
 
-              {/* Phân trang */}
-              <Pagination className="mt-4 justify-content-center">
-                <Pagination.Prev
+                <Form.Group controlId="rating" className="mb-4">
+                  <Form.Label>Đánh Giá</Form.Label>
+                  {[5, 4, 3].map((star) => (
+                    <Form.Check
+                      key={star}
+                      type="checkbox"
+                      label={`${"⭐️".repeat(star)} trở lên`}
+                      onChange={() => setRatingFilter(star)}
+                      checked={ratingFilter === star}
+                    />
+                  ))}
+                </Form.Group>
+
+                <Form.Group controlId="promotion" className="mb-4">
+                  <Form.Label>Dịch Vụ & Khuyến Mãi</Form.Label>
+                  <Form.Check
+                    type="checkbox"
+                    label="Voucher Xtra"
+                    onChange={() => togglePromotion("Voucher Xtra")}
+                    checked={promotions.includes("Voucher Xtra")}
+                  />
+                  <Form.Check
+                    type="checkbox"
+                    label="Đang giảm giá"
+                    onChange={() => togglePromotion("Đang giảm giá")}
+                    checked={promotions.includes("Đang giảm giá")}
+                  />
+                  <Form.Check
+                    type="checkbox"
+                    label="Hàng có sẵn"
+                    onChange={() => togglePromotion("Hàng có sẵn")}
+                    checked={promotions.includes("Hàng có sẵn")}
+                  />
+                </Form.Group>
+
+                <Button
+                  variant="secondary"
+                  className="mt-3 w-100"
+                  onClick={handleResetFilters}
+                >
+                  Xóa Tất Cả
+                </Button>
+              </Form>
+            </div>
+          </Col>
+
+          <Col md={9}>
+            <Row className="align-items-center justify-content-between mb-3">
+              <h5>Kết quả tìm kiếm cho "{keyword}"</h5>
+              <Dropdown onSelect={(e) => handleSortChange(e)}>
+                <Dropdown.Toggle variant="outline-primary">
+                  {/* {sortBy} */}
+                </Dropdown.Toggle>
+                <Dropdown.Menu>
+                  <Dropdown.Item eventKey="Liên Quan">Liên Quan</Dropdown.Item>
+                  <Dropdown.Item eventKey="Giá: Từ Thấp đến Cao">
+                    Giá: Từ Thấp đến Cao
+                  </Dropdown.Item>
+                  <Dropdown.Item eventKey="Giá: Từ Cao đến Thấp">
+                    Giá: Từ Cao đến Thấp
+                  </Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            </Row>
+
+            <Row xs={1} md={4} className="g-3">
+              {loading ? (
+                <>
+                  {Array.from({ length: 40 }).map((_, index) => (
+                    <Col
+                      key={index}
+                      style={{
+                        padding: 0,
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "center",
+                      }}
+                    >
+                      <ProductItem product={null} loading={true}/>
+                    </Col>
+                  ))}
+                </>
+              ) : (
+                <>
+                  {products && (
+                    <>
+                      {products.map((product) => (
+                        <Col
+                          key={product.id}
+                          style={{
+                            padding: 0,
+                            display: "flex",
+                            alignItems: "center",
+                            justifyContent: "center",
+                          }}
+                        >
+                          <ProductItem product={product} loading={false}/>
+                        </Col>
+                      ))}
+                    </>
+                  )}
+                </>
+              )}
+            </Row>
+
+            <Row xs={1} md={4} className="g-3">
+              {products && (
+                <>
+                  {products.map((product) => (
+                    <Col
+                      key={product.id}
+                      style={{
+                        padding: 0,
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "center",
+                      }}
+                    >
+                      <ProductItem product={product} />
+                    </Col>
+                  ))}
+                </>
+              )}
+            </Row>
+
+            {/* Phân trang */}
+            <Pagination className="mt-4 justify-content-center">
+              <Pagination.Prev
+                onClick={() => {
+                  animateScroll.scrollToTop({
+                    duration: 800,
+                    smooth: true,
+                    offset: -70,
+                  });
+                  setTimeout(() => {
+                    url.searchParams.set("page", Math.max(page - 1, 1));
+                    navigate(url.pathname + url.search);
+                  }, 800);
+                }}
+              />
+              {[...Array(totalPages)].map((_, index) => (
+                <Pagination.Item
+                  key={index}
+                  active={index + 1 === page}
                   onClick={() => {
                     animateScroll.scrollToTop({
                       duration: 800,
@@ -272,51 +321,34 @@ const SearchPage = () => {
                       offset: -70,
                     });
                     setTimeout(() => {
-                      url.searchParams.set("page", Math.max(page - 1, 1));
+                      url.searchParams.set("page", index + 1);
                       navigate(url.pathname + url.search);
                     }, 800);
                   }}
-                />
-                {[...Array(totalPages)].map((_, index) => (
-                  <Pagination.Item
-                    key={index}
-                    active={index + 1 === page}
-                    onClick={() => {
-                      animateScroll.scrollToTop({
-                        duration: 800,
-                        smooth: true,
-                        offset: -70,
-                      });
-                      setTimeout(() => {
-                        url.searchParams.set("page", index + 1);
-                        navigate(url.pathname + url.search);
-                      }, 800);
-                    }}
-                  >
-                    {index + 1}
-                  </Pagination.Item>
-                ))}
-                <Pagination.Next
-                  onClick={() => {
-                    animateScroll.scrollToTop({
-                      duration: 800,
-                      smooth: true,
-                      offset: -70,
-                    });
-                    setTimeout(() => {
-                      url.searchParams.set(
-                        "page",
-                        Math.min(page + 1, totalPages)
-                      );
-                      navigate(url.pathname + url.search);
-                    }, 800);
-                  }}
-                />
-              </Pagination>
-            </Col>
-          </Row>
-        </div>
-      )}
+                >
+                  {index + 1}
+                </Pagination.Item>
+              ))}
+              <Pagination.Next
+                onClick={() => {
+                  animateScroll.scrollToTop({
+                    duration: 800,
+                    smooth: true,
+                    offset: -70,
+                  });
+                  setTimeout(() => {
+                    url.searchParams.set(
+                      "page",
+                      Math.min(page + 1, totalPages)
+                    );
+                    navigate(url.pathname + url.search);
+                  }, 800);
+                }}
+              />
+            </Pagination>
+          </Col>
+        </Row>
+      </div>
     </>
   );
 };
