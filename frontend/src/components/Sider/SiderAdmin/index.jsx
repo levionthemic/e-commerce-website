7a3327e3e0ab23ff267551ffd51cc09f66cb7e6f
Ho Tran Ngoc Liem <ngocliem10a5nth@gmail.com>
2024-12-09 21:20:01 +0700

    Sửa lại SiderAdmin

--- frontend/src/components/Sider/SiderAdmin/index.jsx
+++ frontend/src/components/Sider/SiderAdmin/index.jsx
@@ -3,7 +3,7 @@ import "./SiderAdmin.scss";
 import logo from "../../../assets/images/AniCart.png";
 import { axiosApi } from "../../../services/UserService";
 import { prefixAdmin } from "../../../config/system";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 import siderIcon1 from "../../../assets/images/admin/sider-menu-icon-1.svg";
 import siderIcon2 from "../../../assets/images/admin/sider-menu-icon-2.svg";
@@ -16,76 +16,167 @@ import defaultUserIcon from "../../../assets/images/admin/default-user-icon.webp
 
 function SiderAdmin() {
   const [user, setUser] = useState();
+  const navigate = useNavigate();
+  const [navLinksActive, setNavLinksActive] = useState([
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+  ]);
+
   useEffect(() => {
     axiosApi.get("/api/v1" + prefixAdmin + "/user/get-info").then((data) => {
       setUser(data.data.data);
     });
   }, []);
-  console.log(user);
+
+  const pathname = new URL(window.location.href).pathname;
+  useEffect(() => {
+    const temp = [false, false, false, false, false, false, false];
+    switch (pathname) {
+      case `${prefixAdmin}/account`:
+        temp[0] = true;
+        break;
+      case `${prefixAdmin}/dashboard`:
+        temp[1] = true;
+        break;
+      case `${prefixAdmin}/login-log`:
+        temp[2] = true;
+        break;
+      case `${prefixAdmin}/category`:
+        temp[3] = true;
+        break;
+      case `${prefixAdmin}/product`:
+        temp[4] = true;
+        break;
+      case `${prefixAdmin}/user`:
+        temp[5] = true;
+        break;
+      case `${prefixAdmin}/config`:
+        temp[6] = true;
+        break;
+
+      default:
+        break;
+    }
+    setNavLinksActive(temp);
+  }, [pathname]);
+
+  const handleClickMenu = (tab) => {
+    switch (tab) {
+      case "account":
+        navigate(`${prefixAdmin}/account`);
+        break;
+      case "dashboard":
+        navigate(`${prefixAdmin}/dashboard`);
+        break;
+      case "login-log":
+        navigate(`${prefixAdmin}/login-log`);
+        break;
+      case "category":
+        navigate(`${prefixAdmin}/category`);
+        break;
+      case "product":
+        navigate(`${prefixAdmin}/product`);
+        break;
+      case "user":
+        navigate(`${prefixAdmin}/user`);
+        break;
+      case "config":
+        navigate(`${prefixAdmin}/config`);
+        break;
+      default:
+        break;
+    }
+  };
   return (
     <div className="sider-admin">
       <div className="sider-logo">
         <img src={logo} alt="" />
       </div>
-      <div className="sider-info">
-        <Link to={`${prefixAdmin}/account`}>
-          <div className="inner-avatar">
-            <img src={user?.avatar || defaultUserIcon} alt="" />
-          </div>
-          <div className="inner-info">
-            <h6>{user?.fullname}</h6>
-            <span>{user?.email}</span>
-          </div>
-        </Link>
+      <div
+        className={
+          "sider-info " + (navLinksActive[0] ? "sider-info-active" : "")
+        }
+        onClick={() => handleClickMenu("account")}
+      >
+        <div className="inner-avatar">
+          <img src={user?.avatar || defaultUserIcon} alt="" />
+        </div>
+        <div className="inner-info">
+          <h6>{user?.fullname}</h6>
+          <span>{user?.email}</span>
+        </div>
       </div>
       <ul className="sider-menu">
-        <li className="sider-menu-item">
-          <Link to={`${prefixAdmin}/dashboard`}>
-            <div className="inner-icon">
-              <img src={siderIcon1} alt="" />
-            </div>
-            <div className="inner-title">TRANG CHỦ</div>
-          </Link>
+        <li
+          className={
+            "sider-menu-item " + (navLinksActive[1] ? "sider-menu-active" : "")
+          }
+          onClick={() => handleClickMenu("dashboard")}
+        >
+          <div className="inner-icon">
+            <img src={siderIcon1} alt="" />
+          </div>
+          <div className="inner-title">TRANG CHỦ</div>
         </li>
-        <li className="sider-menu-item">
-          <Link to={`${prefixAdmin}/login-log`}>
-            <div className="inner-icon">
-              <img src={siderIcon2} alt="" />
-            </div>
-            <div className="inner-title">LỊCH SỬ ĐĂNG NHẬP</div>
-          </Link>
+        <li
+          className={
+            "sider-menu-item " + (navLinksActive[2] ? "sider-menu-active" : "")
+          }
+          onClick={() => handleClickMenu("login-log")}
+        >
+          <div className="inner-icon">
+            <img src={siderIcon2} alt="" />
+          </div>
+          <div className="inner-title">LỊCH SỬ ĐĂNG NHẬP</div>
         </li>
-        <li className="sider-menu-item">
-          <Link to={`${prefixAdmin}/category`}>
-            <div className="inner-icon">
-              <img src={siderIcon3} alt="" />
-            </div>
-            <div className="inner-title">QUẢN LÝ DANH MỤC</div>
-          </Link>
+        <li
+          className={
+            "sider-menu-item " + (navLinksActive[3] ? "sider-menu-active" : "")
+          }
+          onClick={() => handleClickMenu("category")}
+        >
+          <div className="inner-icon">
+            <img src={siderIcon3} alt="" />
+          </div>
+          <div className="inner-title">QUẢN LÝ DANH MỤC</div>
         </li>
-        <li className="sider-menu-item">
-          <Link to={`${prefixAdmin}/product`}>
-            <div className="inner-icon">
-              <img src={siderIcon4} alt="" />
-            </div>
-            <div className="inner-title">QUẢN LÝ SẢN PHẨM</div>
-          </Link>
+        <li
+          className={
+            "sider-menu-item " + (navLinksActive[4] ? "sider-menu-active" : "")
+          }
+          onClick={() => handleClickMenu("product")}
+        >
+          <div className="inner-icon">
+            <img src={siderIcon4} alt="" />
+          </div>
+          <div className="inner-title">QUẢN LÝ SẢN PHẨM</div>
         </li>
-        <li className="sider-menu-item">
-          <Link to={`${prefixAdmin}/user`}>
-            <div className="inner-icon">
-              <img src={siderIcon6} alt="" />
-            </div>
-            <div className="inner-title">QUẢN LÝ TÀI KHOẢN</div>
-          </Link>
+        <li
+          className={
+            "sider-menu-item " + (navLinksActive[5] ? "sider-menu-active" : "")
+          }
+          onClick={() => handleClickMenu("user")}
+        >
+          <div className="inner-icon">
+            <img src={siderIcon6} alt="" />
+          </div>
+          <div className="inner-title">QUẢN LÝ TÀI KHOẢN</div>
         </li>
-        <li className="sider-menu-item">
-          <Link to={`${prefixAdmin}/config`}>
-            <div className="inner-icon">
-              <img src={siderIcon5} alt="" />
-            </div>
-            <div className="inner-title">QUẢN LÝ CẤU HÌNH</div>
-          </Link>
+        <li
+          className={
+            "sider-menu-item " + (navLinksActive[6] ? "sider-menu-active" : "")
+          }
+          onClick={() => handleClickMenu("config")}
+        >
+          <div className="inner-icon">
+            <img src={siderIcon5} alt="" />
+          </div>
+          <div className="inner-title">QUẢN LÝ CẤU HÌNH</div>
         </li>
       </ul>
     </div>
